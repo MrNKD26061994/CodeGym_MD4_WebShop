cd50a7c881d9564ef4e4a316589ac104959049e3
DuyNguyen <elearning.nkd@gmail.com>
2023-11-01 10:21:22 +0700

    Update tem IGeneralController

--- src/main/java/com/codegym/md4_webshop/controller/UserController.java
+++ src/main/java/com/codegym/md4_webshop/controller/UserController.java
@@ -1,8 +1,5 @@
 package com.codegym.md4_webshop.controller;
 
-import com.codegym.md4_webshop.model.User;
-import com.codegym.md4_webshop.model.cart.Cart;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,24 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin("*")
 @RestController
 @RequestMapping("/user")
-public class UserController implements IGeneralController<User> {
-    @Override
-    public Iterable<Cart> list() {
-        return null;
-    }
+public class UserController{
 
-    @Override
-    public ResponseEntity<Cart> create(User user) {
-        return null;
-    }
-
-    @Override
-    public ResponseEntity<Cart> update(User user) {
-        return null;
-    }
-
-    @Override
-    public ResponseEntity<Cart> delete() {
-        return null;
-    }
 }
