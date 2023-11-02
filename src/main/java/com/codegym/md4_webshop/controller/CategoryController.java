dac4da7c60a4b9d5bd374b656affae2be62046d9
Tung Duong <letungduong2212@gmail.com>
2023-11-02 22:46:29 +0700

    list cate

--- src/main/java/com/codegym/md4_webshop/controller/CategoryController.java
+++ src/main/java/com/codegym/md4_webshop/controller/CategoryController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,8 +28,10 @@ public class CategoryController implements IGeneralController<Category> {
     }
 
     @Override
-    public ResponseEntity create(Object o) {
-        return null;
+    public ResponseEntity create(@RequestBody Category category) {
+        category.setStatus(1);
+        categoryService.save(category);
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @Override
